4ec3b3ab6d93d935fc886413ea66c8af1e27a589
Celia Chen <celiachen0822@gmail.com>
2021-01-26 16:58:51 -0800

    Complete intent example

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.helloworld;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -84,8 +85,37 @@ public class MainActivity extends AppCompatActivity {
     public void launchNextActivity(View view){
         // create an intent and you need to specify from and to
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
 
+        // data field
+        // intent extras -> Celia's choice
+        // both can pack data and send to the targeted activity
+
+        // intent extras:
+        // key/value pairs in a Bundle
+
+        // i want to pass the count number through intent to second activity and display in second activity
+        // 5 was passed.
+
+        String message = textView_count.getText().toString();
+        intent.putExtra("count", message);
+
+        //startActivity(intent);
+        startActivityForResult(intent, 1); // <0 -> reply is not requested
     }
 
+    // do something when the result is received
+    // a lifecycle method
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1){
+            if (resultCode == RESULT_OK){
+                String reply = data.getStringExtra("replyCount");
+                textView_count.setText(reply);
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/helloworld/SecondActivity.java
+++ app/src/main/java/com/example/helloworld/SecondActivity.java
@@ -1,15 +1,72 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private ConstraintLayout constraintLayout;
+    private LinearLayout linearLayout;
+    private Button button_goBack;
+    private String receivedMessage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // display a message
+        // x was passed from main activity.
+
+        // extract intent extras information
+        Intent intent = getIntent();
+        receivedMessage = intent.getStringExtra("count");
+        Log.d("Data from Main Activity", receivedMessage);
+
+        // grab the constraintLayout in second activity
+        constraintLayout = findViewById(R.id.second_root_layout);
+        linearLayout = findViewById(R.id.second_linear_layout);
+        button_goBack = findViewById(R.id.button_goBack);
+
+        // create a textview
+        // set the text to receivedMessage + " was passed from main acitivyt."
+        // add this textView to constraintLayout
+        /**
+        TextView textView = new TextView(this);
+        textView.setText(receivedMessage + " was passed from main activity.");
+        textView.setTextSize(30);
+        constraintLayout.addView(textView);
+         */
+        // if you do not know how many views you will need to create
+        for (int i = 0; i < Integer.parseInt(receivedMessage); i++){
+            TextView textView = new TextView(this);
+            textView.setText("hello");
+            linearLayout.addView(textView);
+        }
+
+        // add clicklistener for button
+        button_goBack.setOnClickListener(v -> {
+            replyIntent(v);
+        });
+    }
+
+    public void replyIntent(View view){
+        // create a reply intent and pack the information, send it back to main activity
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra("replyCount", receivedMessage);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
