6153691e7e38ef883746ae8a00e328aea67f8a6b
Celia Chen <celiachen0822@gmail.com>
2021-01-28 16:49:46 -0800

    HTTP Get request example

--- app/src/main/java/com/example/helloworld/SecondActivity.java
+++ app/src/main/java/com/example/helloworld/SecondActivity.java
@@ -11,9 +11,14 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -21,6 +26,10 @@ public class SecondActivity extends AppCompatActivity {
     private LinearLayout linearLayout;
     private Button button_goBack;
     private String receivedMessage;
+
+
+    private static final String api_url = "https://icanhazdadjoke.com/";
+    private static AsyncHttpClient client = new AsyncHttpClient();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,8 +41,9 @@ public class SecondActivity extends AppCompatActivity {
 
         // extract intent extras information
         Intent intent = getIntent();
+
         receivedMessage = intent.getStringExtra("count");
-        Log.d("Data from Main Activity", receivedMessage);
+        //Log.d("Data from Main Activity", receivedMessage);
 
         // grab the constraintLayout in second activity
         constraintLayout = findViewById(R.id.second_root_layout);
@@ -50,23 +60,75 @@ public class SecondActivity extends AppCompatActivity {
         constraintLayout.addView(textView);
          */
         // if you do not know how many views you will need to create
-        for (int i = 0; i < Integer.parseInt(receivedMessage); i++){
-            TextView textView = new TextView(this);
-            textView.setText("hello");
-            linearLayout.addView(textView);
-        }
+
+            for (int i = 0; i < Integer.parseInt(receivedMessage); i++) {
+                TextView textView = new TextView(this);
+                textView.setText("hello");
+                linearLayout.addView(textView);
+            }
+
 
         // add clicklistener for button
         button_goBack.setOnClickListener(v -> {
-            replyIntent(v);
+            //replyIntent(v);
+            launchNextActivity(v);
         });
+
     }
 
+    /**
     public void replyIntent(View view){
         // create a reply intent and pack the information, send it back to main activity
         Intent replyIntent = new Intent();
         replyIntent.putExtra("replyCount", receivedMessage);
         setResult(RESULT_OK, replyIntent);
         finish();
+    }*/
+
+    public void launchNextActivity(View view){
+
+        // when the button is clicked
+        // I want to send a get request to the API
+        // add the data received from the response to the intent
+        // send it to third activity to be displayed
+
+        // set the header because of the api endpoint
+        client.addHeader("Accept", "application/json");
+        // send a get request to the api url
+        client.get(api_url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                // when you get a 200 status code
+                Log.d("api response", new String(responseBody));
+
+                try {
+                    JSONObject json = new JSONObject(new String(responseBody));
+                    Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                    // add the joke into the intent
+                    intent.putExtra("joke", json.getString("joke"));
+
+                    // convert any json data into a string to put into the intent
+                    // when you receive the intent in the next activity,
+                    // convert it back to the json data
+
+                    startActivity(intent);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                /**
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);*/
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                // when you get a 400-499 status code
+                Log.e("api error", new String(responseBody));
+            }
+        });
+
+
     }
 }
